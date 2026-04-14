5394122510968dd780d36d6e4d948ee081d37b1d
Aidil <refandyakbar1709@gmail.com>
2026-04-14 20:09:52 +0700

    update layout & tambah 1 Doujin baru

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ fetch('data.json')
       container.innerHTML += `
         <div class="card" onclick="goDetail(${item.id})">
             <img src="${item.sampul}">
-            <div class="title">${item.judul}</div>
-            <div class="bahasa">Bahasa:${item.bahasa}</div>
-            <div class="halaman">${item.halaman.length} halaman</div> 
+            <div class="judul">${item.judul}</div>
+            <div class="bahasa">Bahasa : ${item.bahasa}</div>
+            <div class="halaman">${item.halaman.length} halaman</div>
         </div>
       `;
     });
